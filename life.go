0970b940d91f4fb7aa0a4c9a6aedf1736e105d87
Charles Pierse <charlespierse@gmail.com>
2019-09-25 23:08:31 +0100

    Making progress on printing board

--- life.go
+++ life.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
-    
+	
 	//"math/rand"
 )
 
-const w, h = 10, 5
+const w, h = 10, 30
 // GameBoard defines the boards initial width and height
 type GameBoard struct {
 	width, height int
@@ -15,7 +14,7 @@ type GameBoard struct {
 }
 
 func main() {
-	board := &GameBoard{width: w, height: h, cells: "O"}
+	board := &GameBoard{width: w, height: h, cells: "=="}
 	// fmt.Println(board.cells)
 	// board.MakeLife()
 	// fmt.Println(board.cells)
@@ -29,14 +28,22 @@ func (gb *GameBoard) MakeLife() {
 
 //NewGameBoard returns an initialized GameBoard
 func NewGameBoard(w, h int, c string) *GameBoard {
+	fmt.Println("I get called")
 	return &GameBoard{width: w, height: h, cells: c}
 }
 
+// PrintBoard does a thing
 func (gb *GameBoard) PrintBoard() {
-	for i := 0; i < gb.height; i++{
-		x := strings.Repeat(gb.cells, gb.width)
-		fmt.Println(x)
+	board := make([][]string,gb.width*gb.height)
+	for i := range board {
+		board[i] = append(board[i],gb.cells)
 	}
+	
+
+	fmt.Println(board[0][0])
+
+
+
 }
 
 
