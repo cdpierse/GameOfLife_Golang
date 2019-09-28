42c7cf3654052fe145f37edef34e3eb387b2e8f5
Charles Pierse <charlespierse@gmail.com>
2019-09-28 17:25:24 +0100

    Game logic completed, and board print working

--- life.go
+++ life.go
@@ -1,34 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
+	"os"
+	"os/exec"
 	"time"
-
-	"github.com/kr/pretty"
-	//"math/rand"
 )
 
-const w, h = 10, 15
-const lifePct = 0.50
+const x, y = 150, 40 // consts for height and width of board
+const lifePct = 0.30 // % of life the board starts off with 
 
 // Board defines the boards initial width and height
 type Board struct {
-	width, height int
-	cells         [][]bool
+	x, y  int // x = width, y = height
+	cells [][]bool
 }
 
 // NewBoard returns an initialized Board
 // with width w and height h and 2d array of bools is
 // populated on each row with a slice of bools
-func NewBoard(w, h int) *Board {
-	b := &Board{width: w, height: h}
-	b.cells = make([][]bool, h)
+func NewBoard(x, y int) *Board {
+	b := &Board{x: x, y: y}
+	b.cells = make([][]bool, y)
 
-	for i := 0; i < h; i++ {
-		b.cells[i] = make([]bool, w)
+	for i := 0; i < y; i++ {
+		b.cells[i] = make([]bool, x)
 	}
-	IntializeRandomLife(b)
 
 	return b
 
@@ -36,23 +35,24 @@ func NewBoard(w, h int) *Board {
 
 // IntializeRandomLife sets indices of the board
 // that are to be randomly given life on creation
-func IntializeRandomLife(b *Board) Board {
+func (b *Board) IntializeRandomLife() Board {
+
 	rand.Seed(time.Now().UnixNano())
-	totalCells := b.width * b.height                               // total cells in 2d array
+	totalCells := b.x * b.y                                        // total cells in 2d array
 	livingCells := int(math.Ceil((lifePct * float64(totalCells)))) //number of cells to make alive
 
 	for i := 0; i < livingCells; i++ {
 
-		randI := rand.Intn(b.height)
-		randJ := rand.Intn(b.width)
+		randX := rand.Intn(b.x)
+		randY := rand.Intn(b.y)
 
 		// while this index remains true, try another
-		for b.cells[randI][randJ] == true { 
-			randI = rand.Intn(b.height)
-			randJ = rand.Intn(b.width)
+		for b.cells[randY][randX] {
+			randX = rand.Intn(b.x)
+			randY = rand.Intn(b.y)
 		}
 
-		b.cells[randI][randJ] = true
+		b.cells[randY][randX] = true
 
 	}
 
@@ -60,35 +60,125 @@ func IntializeRandomLife(b *Board) Board {
 
 }
 
-func main() {
-	n := NewBoard(w, h)
-	pretty.Println(n.cells)
+// Tick implements the rules of Conways' game of life
+// for each generation by testing against certain conditions
+func (b *Board) Tick() {
+
+	cb := NewBoard(b.x, b.y)
+
+	cb.cells = make([][]bool, len(b.cells))
+	for i := range b.cells {
+		cb.cells[i] = make([]bool, len(b.cells[i]))
+		copy(cb.cells[i], b.cells[i])
+	}
+	for y := 0; y < b.y; y++ {
+		for x := 0; x < b.x; x++ {
+			switch {
+			//less than two live neighbours it dies by underpopulation
+			case cb.cells[y][x] && cb.CheckNeighbours(x, y) < 2:
+				b.cells[y][x] = false
+			//more than three live neighbours it dies of overpopulation
+			case cb.cells[y][x] && cb.CheckNeighbours(x, y) > 3:
+				b.cells[y][x] = false
+			// dead with exactly three live neigbours comes to life by reproduction
+			case !cb.cells[y][x] && cb.CheckNeighbours(x, y) == 3:
+				b.cells[y][x] = true
+			//two or three live neighbours it lives to next generation
+			case cb.cells[y][x] && cb.CheckNeighbours(x, y) == 2 || cb.CheckNeighbours(x, y) == 3:
+				b.cells[y][x] = true
+
+			default:
+				b.cells[y][x] = false
+			}
+		}
+	}
 }
 
-// func main() {
-// 	board := &Board{width: w, height: h, cells: "=="}
-// 	// fmt.Println(board.cells)
-// 	// board.MakeLife()
-// 	// fmt.Println(board.cells)
-// 	board.PrintBoard()
-// }
+// CheckNeighbours tests the 8 possible surrounding neighbours
+// of a cell and determines if the index is legal
+// and if the cell is living, in which case livingcount is incremented
+func (b *Board) CheckNeighbours(x, y int) int {
+	livingCount := 0
 
-// // MakeLife is associated with Board and gives life to the cells
-// func (gb *Board) MakeLife() {
-// 	gb.cells = "X"
-// }
+	if b.CheckLegalRange(y-1, x) {
+		livingCount++
+	}
 
-// // PrintBoard does a thing
-// func (gb *Board) PrintBoard() {
-// 	board := make([][]string,gb.width*gb.height)
-// 	for i := range board {
-// 		board[i] = append(board[i],gb.cells)
-// 	}
+	if b.CheckLegalRange(y+1, x) {
+		livingCount++
+	}
 
-// 	fmt.Println(board[0][0])
+	if b.CheckLegalRange(y, x-1) {
+		livingCount++
+	}
 
-// }
+	if b.CheckLegalRange(y, x+1) {
+		livingCount++
+	}
 
-// func BoardArray(w,h int) [][]string{
-// 	returng
-// }
+	if b.CheckLegalRange(y-1, x-1) {
+		livingCount++
+	}
+
+	if b.CheckLegalRange(y-1, x+1) {
+		livingCount++
+	}
+
+	if b.CheckLegalRange(y+1, x-1) {
+		livingCount++
+	}
+	if b.CheckLegalRange(y+1, x+1) {
+		livingCount++
+	}
+	return livingCount
+}
+
+// CheckLegalRange makes sure that the index being tested
+// is legal on our board
+func (b *Board) CheckLegalRange(y, x int) bool {
+	return (x >= 0 && x < b.x && y >= 0 && y < b.y && b.cells[y][x] == true)
+
+}
+// PrintBoard displays the current state of the board graphically in the CLI
+func (b *Board) PrintBoard() {
+	fmt.Print("+")
+	for x := 1; x <= b.x; x++ {
+		fmt.Print("-")
+	}
+	fmt.Println("+")
+
+	for y := 0; y < b.y; y++ {
+		fmt.Print("|")
+		for x := 0; x < b.x; x++ {
+			if b.cells[y][x] {
+				fmt.Print("▓")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println("|")
+	}
+
+	fmt.Print("+")
+	for x := 1; x <= b.x; x++ {
+		fmt.Print("-")
+	}
+	fmt.Println("+")
+}
+
+func main() {
+	n := NewBoard(x, y)
+	n.IntializeRandomLife()
+	for i := 0; i > -1; i++ {
+		
+		clrCmd := exec.Command("clear")
+		clrCmd.Stdout = os.Stdout
+		clrCmd.Run()
+		n.Tick()
+		n.PrintBoard()
+		fmt.Printf("Current Gen:%v",i)
+		time.Sleep(time.Second / 20)
+
+	}
+
+}
